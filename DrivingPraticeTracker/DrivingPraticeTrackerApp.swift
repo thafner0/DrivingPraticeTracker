6a9b5d19a36462777df53ad56ea5c71bc468be85
Trevor Hafner <thafner@luc.edu>
2026-04-13 11:37:54 -0500

    Initial Commit

---
+++ DrivingPraticeTracker/DrivingPraticeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivingPraticeTrackerApp.swift
+//  DrivingPraticeTracker
+//
+//  Created by Trevor Hafner on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct DrivingPraticeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrivingPraticeTrackerTests/DrivingPraticeTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  DrivingPraticeTrackerTests.swift
+//  DrivingPraticeTrackerTests
+//
+//  Created by Trevor Hafner on 4/13/26.
+//
+
+import Testing
+@testable import DrivingPraticeTracker
+
+struct DrivingPraticeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
